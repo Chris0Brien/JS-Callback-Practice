de0e5d4a74dbd93d17ba2f4885eeae0ba8a9099c
Chris O'Brien <you@example.comchristopherwobrien03@gmail.com>
2022-02-05 09:58:04 -0600

    it's alive

--- move.js
+++ move.js
@@ -1,31 +1,31 @@
 function move(element) {
     element.style.position = 'fixed'
-    
+
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
-    
+
     function moveWithArrowKeys(left, bottom, callback){
         let direction = null;
         let x = left;
         let y = bottom;
-        
+    
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
         
         function moveCharacter(){ 
             if(direction === 'west'){
-                x-=1
+                x = x - 1
             }
             if(direction === 'north'){
-                y+=1
+                y= y + 1
             }
             if(direction === 'east'){
-                x+=1
+                x= x + 1
             }
             if(direction === 'south'){
-                y-=1
+                y = y - 1
             }
             element.style.left = x + 'px'
             element.style.bottom = y + 'px'
@@ -48,35 +48,17 @@ function move(element) {
             if(e.key === 'ArrowDown'){
                 direction = 'south'
             }
-            callback(direction)
+            directionChange(direction)
         })
         
         document.addEventListener('keyup', function(e){
             direction = null
-            callback(direction)
+            directionChange(direction)
         })
     }
-    
+
     return {
         to: moveToCoordinates,
         withArrowKeys: moveWithArrowKeys
-    }        
-}
-
-function handleDirectionChange(direction){
-    if(direction === null){
-        character.src = 'assets/green-character/static.gif'
-    }
-    if(direction === 'west'){
-        character.src = 'assets/green-character/west.gif'
-    }
-    if(direction === 'north'){
-        character.src = 'assets/green-character/north.gif'
-    }
-    if(direction === 'east'){
-        character.src = 'assets/green-character/east.gif'
-    }
-    if(direction === 'south'){
-        character.src = 'assets/green-character/south.gif'
     }
 }
